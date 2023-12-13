226eed34152b5656786497a03767bee9558eb1c7
baso-123 <153715467+baso-123@users.noreply.github.com>
2023-12-13 16:23:48 +0800

    Upload file 1

---
+++ praktikum-b1.py
@@ -0,0 +1,82 @@
+print('praktikum-1')
+print('Nama Lengkap : Baso Rahmat Hidayah. R')
+print('NIM          : 231031041')
+print('Prodi        : Sistem Informasi')
+
+print()
+angkatan = 2023
+print('Angkatan adalah',str(angkatan))
+
+print()
+a = 20
+print('Data adalah',a)
+print('Tipe data adalah',type(a))
+
+print()
+b = 20.0
+print('Data adalah',b)
+print('Tipe data adalah',type(b))
+
+print()
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah',kampus)
+print('Tipe data adalah',type(kampus))
+
+print()
+log = True
+print('Data adalah',log)
+print('Tipe data adalah',type(log))
+
+print()
+c = complex(2,3)
+print('Data adalah',c)
+print('Tipe data adalah',type(c))
+
+#penjumlahan
+print()
+p = 10
+q = 5
+hasil = p+q
+print('Hasil',p,'+',q,'=',hasil)
+
+#pengurangan
+print()
+p = 10
+q = 5
+hasil = p-q
+print('Hasil',p,'-',q,'=',hasil)
+
+#perkalian
+print()
+p = 10
+q = 5
+hasil = p*q
+print('Hasil',p,'*',q,'=',hasil)
+
+#pembagian
+print()
+x = 20
+y = 2
+hasil = x/y
+print('Hasil',x,'/',y,'=',hasil)
+
+#modulus
+print()
+x = 10
+y = 2
+hasil = x%y
+print('Hasil',x,'%',y,'=',hasil)
+
+#eksponen
+print()
+x = 10
+y = 2
+hasil = x**y
+print('Hasil',x,'**',y,'=',hasil)
+
+#floordivision
+print()
+x = 40
+y = 2
+hasil = x//y
+print('Hasil',x,'//',y,'=',hasil)
