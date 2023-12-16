9b0fb28a82f9e60c0a4a39c69baab4c90855d431
baso-123 <153715467+baso-123@users.noreply.github.com>
2023-12-16 21:00:04 +0800

    Upload file tugas 9

---
+++ 231031041-baso rahmat hidayah. r-tugas-9.py
@@ -0,0 +1,17 @@
+biodata = {'nama lengkap'         : 'Baso Rahmat Hidayah. R',
+        'nama panggilan'       : 'Baso',
+        'jenis kelamin'        : 'Laki-laki',
+        'tempat/tanggal lahir' : 'Atakkae, 3 Mei 2005',
+        'agama'                : 'Islam',
+        'alamat'               : 'Grand Hill 2 Residence Blok AA No. 5',
+        'program studi'        : 'Sistem Informasi',
+        'angkatan'             : '2023',
+        'nomor telepon'        : '085246560978',
+        'email'                : 'basoramhat11@gmail.com',
+        }
+
+print('Nama                 :' , biodata['nama lengkap'])
+print('Tempat/tanggal lahir :' , biodata['tempat/tanggal lahir'])      
+print('Jenis Kelamin        :' , biodata['jenis kelamin'])
+print('Program Studi        :' , biodata['program studi'])
+print('Angkatan             :' , biodata['angkatan'])
