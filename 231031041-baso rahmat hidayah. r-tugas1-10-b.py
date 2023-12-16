ac37477ce3ea703f7ab59488c76571df892c1e95
baso-123 <153715467+baso-123@users.noreply.github.com>
2023-12-16 21:08:37 +0800

    Upload file tugas 10

---
+++ 231031041-baso rahmat hidayah. r-tugas1-10-b.py
@@ -0,0 +1,8 @@
+print('Tugas 1 : Buat Program\n')
+
+x = int(input('masukkan nilai : '))
+
+if x % 2 == 1 :
+    print('Bilangan X adalah Ganjil')
+else:
+    print('Bilangan X adalah Bukan Ganjil')
