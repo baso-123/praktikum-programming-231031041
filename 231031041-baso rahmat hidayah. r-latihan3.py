42d13f95bf51eac65be87312de5c3ea732fc650f
baso-123 <153715467+baso-123@users.noreply.github.com>
2023-12-16 21:18:52 +0800

    Upload file tugas 13

---
+++ 231031041-baso rahmat hidayah. r-latihan3.py
@@ -0,0 +1,8 @@
+Nama       = str(input('Nama Karyawan = '))
+Pendapatan = float(input('Pendapatan karyawan = '))
+
+if Pendapatan > 1000 :
+    print('Status = Berhak')
+else :
+    print('Status = Tidak Berhak')
+
