464db8ddb634bf793c29b02bd716b57c0744467d
baso-123 <153715467+baso-123@users.noreply.github.com>
2023-12-16 20:49:33 +0800

    Upload file 6

---
+++ Praktikum-b6.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+a = True
+i = 0
+limit = 5
+
+while a :
+    i += 1 # i = i + 1
+    if i <= limit :
+        print('Selamat Bergabung',limit-i+1)
+    else :
+        a = False
